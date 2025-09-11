555583f52f171fe573d1f92c8820151a159a9bce
sandithS <sandithsemuthu140@gmail.com>
2025-09-11 18:31:09 +0530

    Create Customer Dashboard buttons OnAction methods

--- src/main/java/controller/CustomerDashboardFormController.java
+++ src/main/java/controller/CustomerDashboardFormController.java
@@ -3,6 +3,11 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerDashboardFormController {
 
@@ -18,24 +23,46 @@ public class CustomerDashboardFormController {
     @FXML
     private JFXButton btnOrderDetails;
 
+    Stage stage = new Stage();
+
     @FXML
     void btnCustomerOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     @FXML
     void btnItemOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     @FXML
     void btnOrderDetailsOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_details_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     @FXML
     void btnOrderOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
 }
