84f8a94bca78da6981e11c56df723cff141b9eb3
sandithS <sandithsemuthu140@gmail.com>
2025-09-11 18:22:50 +0530

    Add other Forms and Controller classes

--- src/main/java/controller/CustomerDashboardFormController.java
+++ src/main/java/controller/CustomerDashboardFormController.java
@@ -1,4 +1,41 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class CustomerDashboardFormController {
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnItem;
+
+    @FXML
+    private JFXButton btnOrder;
+
+    @FXML
+    private JFXButton btnOrderDetails;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
 }
