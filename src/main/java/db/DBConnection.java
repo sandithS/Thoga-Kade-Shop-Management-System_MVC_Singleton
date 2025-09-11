e6125b55b6afafd8ec24010ec05918462a33335e
sandithS <sandithsemuthu140@gmail.com>
2025-09-11 19:54:52 +0530

    Create DBConnection class and add mysql connector dependecy to connect with database

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,28 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static DBConnection instance;
+
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Thogakade","root","1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (instance == null){
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+}
