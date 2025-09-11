d3a8bb0cb8e8023e10f55a5732c025eea5284f10
sandithS <sandithsemuthu140@gmail.com>
2025-09-11 18:53:08 +0530

    Design ui for Customer form and implements methods for buttons OnAction

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,4 +1,109 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerFormController {
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnLoad;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colCustId;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colProvince;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtCity;
+
+    @FXML
+    private JFXTextField txtCustId;
+
+    @FXML
+    private JFXTextField txtDob;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPostalCode;
+
+    @FXML
+    private JFXTextField txtProvince;
+
+    @FXML
+    private JFXTextField txtSalary;
+
+    @FXML
+    private JFXTextField txtTitle;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
